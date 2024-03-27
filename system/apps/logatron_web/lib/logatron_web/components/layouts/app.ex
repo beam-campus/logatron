87b408e830c63c34ef57da439d6661e795b86c4a
Rafael Lefever <rafael.lefever@discomco.pl>
2024-03-27 19:03:54 +0100

    added menu animation

--- system/apps/logatron_web/lib/logatron_web/components/layouts/app.ex
+++ system/apps/logatron_web/lib/logatron_web/components/layouts/app.ex
@@ -5,8 +5,18 @@ defmodule LogatronWeb.Layouts.App do
   alias Phoenix.LiveView.JS
 
   def toggle_dropdown_menu do
-    JS.toggle(to: "#dropdown_menu")
+    JS.toggle(
+      to: "#dropdown_menu",
+      in: {
+        "transition ease-out duration-300",
+        "transform opacity-0 translate-y-[-10%]",
+        "transform opacity-100 translate-y-0"
+      },
+      out: {
+        "transition ease-in duration-300",
+        "transform opacity-100 translate-y-0",
+        "transform opacity-0 translate-y-[-10%]"
+      }
+    )
   end
-
-
 end
